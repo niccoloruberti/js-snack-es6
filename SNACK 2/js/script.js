f9334ee5b387b07eef72346f99e5d3257dd4e564
niccoloruberti <nikiruberti@gmail.com>
2023-06-01 17:41:49 +0200

    snack2 finito

--- SNACK 2/js/script.js
+++ SNACK 2/js/script.js
@@ -1,1 +1,48 @@
 'use strict';
+
+const students = [
+    {'id': '213',
+    'name': 'Marco della Rovere',
+    'grades': '78'},
+    {'id': '110',
+    'name': 'Paola Cortellessa',
+    'grades': '96'},
+    {'id': '250',
+    'name': 'Andrea Mantegna',
+    'grades': '48'},
+    {'id': '145',
+    'name': 'Gaia Borromini',
+    'grades': '74'},
+    {'id': '196',
+    'name': 'Luigi Grimaldello',
+    'grades': '68'},
+    {'id': '102',
+    'name': 'Piero della Francesca',
+    'grades': '50'},
+    {'id': '120',
+    'name': 'Francesca da Polenta',
+    'grades': '84'},
+];
+
+//creo un nuovo array con i nomi degli studenti in maiuscolo
+const upperCaseStudents = students.map((item) => {
+    return item.name.toUpperCase();
+});
+
+console.log(upperCaseStudents);
+
+//creo un nuovo array con gli studenti che hanno un valore del totale dei voti superiore a 70
+const over70Students = students.filter((item) => {
+    if(item.grades > 70) {
+        return true;
+    };
+});
+console.log(over70Students);
+
+//creo un nuovo array con gli studenti che hanno un valore totale dei voti compreso tra 70 e 120 (estremi esclusi)
+const over70Under120Students = students.filter((item) => {
+    if(item.grades > 70 && item.grades < 120) {
+        return true;
+    };
+});
+console.log(over70Under120Students)
