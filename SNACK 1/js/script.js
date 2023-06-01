59e1bb7c2049610b4cc77dcd0e3457d60ed42148
niccoloruberti <nikiruberti@gmail.com>
2023-06-01 16:48:16 +0200

    snack1 finito

--- SNACK 1/js/script.js
+++ SNACK 1/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+//scrivo l'array con i nomi degli invitati
+const guests = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+console.log(guests);
+
+//creo un nuovo array con le informazioni aggiuntive
+const objGuests = guests.map((item, index) => {
+    console.log(item);
+    console.log(index)
+    let obj = {
+        name: item,
+        tavolo: 'Tavolo Vip',
+        posto: index + 1,
+    };
+    return obj
+});
+console.log(objGuests);
